e5cca86eaac2316ed3bd93bf2386b223924ee082
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-07-08 02:22:57 -0300

    processamento de dados e casting

---
+++ processamento-de-dados-e-casting/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int x1, y1;
+    x1 = 5;
+    y1 = 2 * x1;
+    cout << x1 << endl;
+    cout << y1 << endl;
+
+    int x2;
+    double y2;
+    x2 = 5;
+    y2 = 2 * x2;
+    cout << x2 << endl;
+    cout << fixed << setprecision(1) << y2 << endl;
+
+    double b1, b2, h, area;
+    b1 = 6.0;
+    b2 = 8.0;
+    h = 5.0;
+    area = (b1 + b2) / 2.0 * h;
+    cout << area << endl;
+
+    int a, b, resultado;
+    a = 5;
+    b = 2;
+    resultado = a / b;
+    cout << resultado << endl;
+
+    double a3;
+    int b3;
+    a3 = 5.0;
+    b3 = (int) a3;
+    cout << b3 << endl;
+
+    return 0;
+}
