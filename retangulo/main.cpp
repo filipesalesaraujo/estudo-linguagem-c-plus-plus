eecd863829cf668bda17e6f5332a830d039cf752
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-07-08 02:54:45 -0300

    retangulo

---
+++ retangulo/main.cpp
@@ -0,0 +1,31 @@
+//Fazer um programa para ler as medidas da base e altura de um retângulo. Em seguida, mostrar o valor
+//da área, perímetro e diagonal deste retângulo, com quatro casas decimais, conforme exemplos.
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+
+    double  base, altura, area, perimetro, diagonal;
+
+    cout << "Base do retangulo: ";
+    cin >> base;
+
+    cout << "Altura do retangulo: ";
+    cin >> altura;
+
+    area = base * altura;
+    perimetro = 2 * (base + altura);
+    diagonal = sqrt(pow(base, 2.0) + pow(altura, 2.0));
+
+    cout << fixed << setprecision(4);
+    cout << "AREA = " << area << endl;
+    cout << "PERIMETRO = " << perimetro << endl;
+    cout << "DIAGONAL = " << diagonal << endl;
+
+    return 0;
+}
