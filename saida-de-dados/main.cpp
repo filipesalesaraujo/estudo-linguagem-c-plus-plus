2d07798493d3414b147d589f348544b330547fc0
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-07-08 01:49:28 -0300

    saída de dados

---
+++ saida-de-dados/main.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+int main()
+{
+
+    cout << "Bom dia";
+    cout << "Boa noite\n";
+
+    cout << "Bom dia" << endl;
+    cout << "Boa noite" << endl;
+
+    int x1, y1;
+    x1 = 10;
+    y1 = 20;
+    cout << x1 << endl;
+    cout << y1 << endl;
+
+    double x2;
+    x2 = 2.3456;
+    cout << fixed << setprecision(2) << x2 << endl;
+
+    int idade;
+    double salario;
+    string nome;
+    char sexo;
+    idade = 32;
+    salario = 4560.9;
+    nome = "Maria Silva";
+    sexo = 'F';
+    cout << fixed << setprecision(2);
+    cout << "A funcionaria " << nome << ", sexo " << sexo << ", ganha " << salario << " e tem " << idade << " anos" << endl;
+
+    return 0;
+}
